590ba2830c7b56f70cc4ac6282223f19286d5268
Spencer Alves <impiaaa@gmail.com>
2012-06-18 22:31:45 +0000

    Imported from Dropbox Classes/SporepediaTableViewController.h at Mon, 18 Jun 2012 22:31:45 +0000

---
+++ Classes/SporepediaTableViewController.h
@@ -0,0 +1,54 @@
+//
+//  SporepediaTableViewController.h
+//  Sporepedia
+//
+//  Created by Spencer Alves on 2/21/09.
+//  Copyright 2009 Spencer Alves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SporepediaTableViewController : UITableViewController<NSXMLParserDelegate> {
+	NSMutableArray *data;
+	UIImage *loadingImage;
+	NSString *searchTerm;
+	NSString *currentTag;
+	NSMutableString *tempData;
+	NSMutableData *xmlData;
+	NSURLConnection *xmlConnection;
+	NSXMLParser *parser;
+	NSString *searchType;
+}
+
+- (void)startDownload;
+- (void)viewWillAppear:(BOOL)animated;
+- (void)viewWillDisappear:(BOOL)animated;
+- (void)didReceiveMemoryWarning;
+- (void)dealloc;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)newIndexPath;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView;
+- (void) parser:(NSXMLParser *)parser
+didStartElement:(NSString *)elementName
+   namespaceURI:(NSString *)namespaceURI
+  qualifiedName:(NSString *)qualifiedName
+	 attributes:(NSDictionary *)attributeDict;
+- (void)parser:(NSXMLParser *)parser
+ didEndElement:(NSString *)elementName
+  namespaceURI:(NSString *)namespaceURI
+ qualifiedName:(NSString *)qName;
+- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string;
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response;
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)recievedData;
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection;
+- (void)scrollViewDidScrollToTop:(UIScrollView *)scrollView;
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate;
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView;
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error;
+
+@property (nonatomic, retain) NSString *searchTerm;
+@property (nonatomic, retain) NSString *searchType;
+@property (nonatomic, retain) NSMutableArray *data;
+
+@end
